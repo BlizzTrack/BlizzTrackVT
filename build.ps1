2f6f3335a2f27700633088ee4c5f784558f7f4b2
Helba <Toyz@users.noreply.github.com>
2020-10-14 20:10:30 -0700

    Delete build.ps1

--- build.ps1
+++
@@ -1,7 +0,0 @@
-docker build --rm --file '.\BlizzTrackVT\Dockerfile' . --tag registry.gitlab.com/toyz/vt:latest
-docker build --rm --file '.\BTVT_Worker\Dockerfile' . --tag registry.gitlab.com/toyz/vt/worker:latest
-
-docker push registry.gitlab.com/toyz/vt:latest
-docker push registry.gitlab.com/toyz/vt/worker:latest
-
-docker image prune -f
