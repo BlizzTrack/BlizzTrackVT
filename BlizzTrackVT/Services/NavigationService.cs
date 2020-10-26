b6b12120dde131b41f67dde97725f68a2eb64efa
Helba The AI <toyzz@live.com>
2020-10-26 13:28:47 -0700

    minor clean up and added support for missing games

--- BlizzTrackVT/Services/NavigationService.cs
+++ BlizzTrackVT/Services/NavigationService.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using BNetLib.Helpers;
+using BNetLib.Models;
 
 namespace BlizzTrackVT.Services
 {
     public class NavigationService
     {
-        public Dictionary<string, List<BNetLib.Models.Summary>> Create(List<BNetLib.Models.Summary> items)
+        public Dictionary<string, List<Summary>> Create(List<Summary> items)
         {
-            var p = BNetLib.Helpers.GameName.Prefix.Keys.OrderBy(x => x);
+            var p = GameName.Prefix.Keys.OrderBy(x => x).ToList();
 
-            var res = new Dictionary<string, List<BNetLib.Models.Summary>>();
+            var res = new Dictionary<string, List<Summary>>();
+
+            var itemsAdded = new List<Summary>();
 
             foreach (var prefix in p)
             {
-                var i = new List<BNetLib.Models.Summary>();
+                var i = new List<Summary>();
 
                 switch (prefix)
                 {
@@ -23,27 +27,43 @@ namespace BlizzTrackVT.Services
                     case "catalogs":
                         continue;
                     case "bna":
-                        i.AddRange(items.Where(x => x.Product == "bna"));
-                        i.AddRange(items.Where(x => x.Product == "catalogs"));
-                        i.AddRange(items.Where(x => x.Product == "agent"));
-                        i.AddRange(items.Where(x => x.Product == "bts"));
+
+                        var its = items.Where(x => x.Product == "bna" || x.Product == "catalogs" || x.Product == "agent" || x.Product == "bts");
+                        var collection = its as Summary[] ?? its.ToArray();
+                        i.AddRange(collection);
+                        itemsAdded.AddRange(collection);
+
                         res.Add(prefix, i);
 
                         continue;
                 }
 
-                foreach (var item in items)
+                foreach (var item in items.Where(item => item.Product.ToLower().Replace("_", "")
+                    .StartsWith(prefix, StringComparison.CurrentCultureIgnoreCase)))
                 {
-                    if (item.Product.ToLower().Replace("_", "").StartsWith(prefix, StringComparison.CurrentCultureIgnoreCase))
+                    if (item.Product.StartsWith("wow_classic") && prefix == "wow")
                     {
-                        if(item.Product.StartsWith("wow_classic") && prefix == "wow") continue;
-                        i.Add(item);
+                        itemsAdded.Add(item);
+                        continue;
                     }
+
+                    itemsAdded.Add(item);
+                    i.Add(item);
                 }
 
-                if(i.Count > 0)
+                if (i.Count > 0)
                     res.Add(prefix, i);
             }
+
+            foreach (var item in itemsAdded)
+            {
+                items.Remove(item);
+            }
+
+            if (items.Count <= 0) 
+                return res;
+
+            res.Add("Unknown", items.ToList());
 
             return res;
         }
